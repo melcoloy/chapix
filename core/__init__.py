593fe978b11b7a42f4b676a4801641880b1c20c7
melcoloy <melcoloy@gmail.com>
2026-04-16 21:24:55 +0200

    zz

--- core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Package core — logique métier du générateur de mosaïques en dominos.
-
-Usage :
-    from core.inventaire import generer_stock, completer_inventaire
-    from core.image import preparer_image, image_vers_matrice, dessiner_mosaique
-    from core.algorithmes import glouton, hongrois, recuit, calculer_score
-"""
